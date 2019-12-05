c371d160d8dfbbae92553fce9e8aa11abe1553bd
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-05 00:01:14 +0000

    Built Scraper Class

--- lib/bandsintown/cli.rb
+++ lib/bandsintown/cli.rb
@@ -6,46 +6,39 @@ class BandsInTown::CLI
     # Scraper.scrape_bands to have my program scrape all of the bands associated with the site
     
     get_location  
-    
     # the program with take in the user input
     
     list_bands  
-    
     # a numbered list of bands playing shows at the location input by the user is returned
     
-    show_details 
-    
+    show_details
     # a user can select a number to view details about the show like date, doors/show time, price, where to buy tickets, and venue
   end
   
   # any method within my call method will be instantiated upon running the program
   
   def get_location
-    
-    # I want a user to enter the name of their city, or allow the computer access to their locatioin to view the bands playing shows in their area
-    
     puts "Enter your City or Location to view a list of bands playing shows in your area:"
-    
     input = gets.strip.to_i 
-    
+    # I want a user to enter the name of their city, or allow the computer access to their location to view the bands playing shows in their area
     # gets returns a string object read from standard input
     # strip will remove any whitespaces
-  end 
+  end
   
-  #def valid.input?(input)
+  # def valid.input?(input)
     # check to see if input is between 1 and the number of bands returned based on user input
-    #input.between?(1, Bands.all.size) (I still need to create a Bands class)
-  #end
+    # input.between?(1, Bands.all.size) (I still need to create a Bands class)
+  # end
   
   def list_bands
-    puts "Bands In Town:"
-    puts <<-DOC     # here doc allows me to specify a string as a block of text where new lines and indents are preserved
+    puts "Events Near You:"
+    puts <<-DOC     
       1. Billie Eilish  
       2. Post Malone
       3. Galantis
     DOC
-    
-    #in between the DOCs is where I want to see a returned list of bands playing shows in the area of the location input by the user
+    # here doc allows me to specify a string as a block of text where new lines and indents are preserved
+    # in between the DOCs is where I want to see a returned list of bands playing shows in the area of the location input by the user
   end
   
   
@@ -54,11 +47,11 @@ class BandsInTown::CLI
     puts "Choose a number to view more details about that show:"
     input = gets.strip.to_i
     # return the show details of the number selected if it is a valid input
-  # if valid.input?(input)
+    # if valid.input?(input)
     puts "Great choice!"
-  # else
-  #  puts "Sorry, not a valid choice!"
-  # end
+    # else
+    #  puts "Sorry, not a valid choice!"
+    # end
   end 
   
 end
---
+++ lib/bandsintown/scraper.rb
@@ -0,0 +1,8 @@
+class Scraper
+  
+  def self.scrape_bands 
+    site = "https://www.bandsintown.com/"
+    html = open(site) #open method can open a file, I'm passing the site in to open
+    doc = Nokogiri::HMTL(html) #nokogiri is a reader parser, I am specifying HTML and passing in the html to be parsed
+  end
+end
