03fde2715de62799d76bbc9c0f85b5ba3c303cc9
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-06 16:15:30 +0000

    pseudocode and file updates

--- lib/songkick.rb
+++ lib/songkick.rb
@@ -3,7 +3,11 @@
 
 require 'nokogiri' # gives us code in node modules allowing us to be able to look through and grab the information we need
 require 'open-uri' # gives us the ability to open a site inside of our code
+# require 'pry'
 
 require_relative './songkick/scraper'
 require_relative './songkick/version'
 require_relative './songkick/cli'
+
+# require_relative used for files required within my program
+# require used for gems installed locally on my computer
--- lib/songkick/cli.rb
+++ lib/songkick/cli.rb
@@ -3,43 +3,36 @@
 class SongKick::CLI 
   
   def run
+   # site = "https://www.songkick.com/metro-areas/6404-us-denver"
+   # html = open(site)
+   # doc = Nokogiri::HTML(html)
+   
     # Scraper.scrape_bands to have my program scrape all of the bands associated with the site
-    
-    get_location  
-    # the program with take in the user input
-    
-    
-    show_details
-    # a user can select a number to view details about the show like venue name and address, event date and time, and where to buy tickets
+    view_event_details  
+   
     
   end
   
   # any method within my run method will be instantiated upon running the program
   
-  def get_location
-    puts "Choose a number to select a city:"
-    puts <<-DOC     
-      1. Denver, CO, US 
-      2. SF Bay Area, CA, US
-      3. Portland, OR, US
-    DOC
+  def view_event_details
+    puts "Choose a number to view more details about an event:"
+    # user enters the number that corresponds with the show they'd like to view more info about
     input = gets.strip.to_i 
-    # user enters the number that corresponds with the city they'd like to view shows in
     # gets returns a string object read from standard input
     # strip will remove any whitespaces
-  end
-  
-  
-  def show_details
-    # prompt user to enter a number associated with a band or an artist
-    puts "Choose a number to view event info:"
-    input = gets.strip.to_i
+    
     # i want to scrape here to return the show details of the number selected if it is a valid input
     # if valid_input?(input)
     puts "Great choice!"
     # else
     #  puts "Sorry, not a valid Choice."
     # end
+  end
+  
+  
+  def buy_tickets
+    puts "Buy tickets"
   end 
   
   # i want a loop of some sort so the program doesn't exit until being asked to
