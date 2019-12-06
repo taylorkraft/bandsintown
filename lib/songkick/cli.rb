848dab135f140bf80b023f18fc8995611cc79983
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-06 22:25:10 +0000

    scraper - scrape venue info

--- lib/songkick/cli.rb
+++ lib/songkick/cli.rb
@@ -1,26 +1,27 @@
 # this class is my CLI controller, responsible for user interactions
 
-class SongKick::CLI 
-  
+class SongKick::CLI
+
   def run
    # site = "https://www.songkick.com/metro-areas/6404-us-denver"
    # html = open(site)
    # doc = Nokogiri::HTML(html)
    Scraper.scrape_denver # to have my program scrape all of the bands associated with the site
    #the code above is encapsulated in a method in the scraper class called scrape_denver
-    choose_event  
-   
-    
+   choose_event
+
+
+
   end
-  
+
   def choose_event
     puts "Choose a number to view more details about that event:"
     # user enters the number that corresponds with the show they'd like to view more info about
-    input = gets.strip.to_i 
+    input = gets.strip.to_i
     # gets returns a string object read from standard input
     # to_i will convert the string object to an integer
     # strip will remove any whitespaces
-    
+
     # i want to scrape here to return the show details of the number selected if it is a valid input
     # if valid_input?(input)
     puts "Great choice!"
@@ -28,22 +29,22 @@ class SongKick::CLI
     #  puts "Sorry, not a valid Choice." < shouldn't need this because it is defined in valid_input? method
     # end
   end
-  
+
   def valid_input?(input)
     # check to see if input is between 1 and the number of artists returned based on user input
    until valid_input.between?(1, Artist.all.size)
     puts "Sorry, not a valid choice. Choose 1 - #{Artist.all.size}"
    end
   end
-  
+
   def buy_tickets
     puts "Choose a number to view ticket pricing and availability from that site:"
     input = gets.strip.to_i
-      
-  end 
-  
+
+  end
+
   # i want a loop of some sort so the program doesn't exit until being asked to
-  
-   
-  
+
+
+
 end
--- lib/songkick/scraper.rb
+++ lib/songkick/scraper.rb
@@ -1,7 +1,7 @@
 class Scraper
-  
-  def self.scrape_denver 
-    site = "https://www.songkick.com/metro-areas/6404-us-denver" 
+
+  def self.scrape_denver
+    site = "https://www.songkick.com/metro-areas/6404-us-denver"
     html = open(site) # open method can open a file, allows me to open the site i've passed in
     doc = Nokogiri::HTML(html) # nokogiri is a reader parser, i am specifying HTML and passing in the site we just opened in order for nnokogiri to parse that site's html
     array_of_artists = doc.css("p.artists.summary") # i am now taking our parsed html and specifying the css selector where my artists names are held - within a <p> tag that holds an artists summary class - this returns an array with all of the information about the artists
@@ -9,8 +9,8 @@ class Scraper
     section.css("strong").text
     # i am iterating over the array_of_artists using map to return a new array where each element is the result of evaluating the css selector "strong" where my arists names are held. this allows the user to receive a completely new list of artists at any given point in time
     # text method will return just our artists names in an array, rather than an array with loads of other information
-   end 
-   
+   end
+
    puts "Concerts in Denver"
    artist_names.each.with_index(1) do |name, i|
      puts "#{i}. #{name}"
@@ -19,10 +19,16 @@ class Scraper
    # the result of this iteration will 'puts' out a list of artist names and a corresponding index number
    # i have interpolated index and name to allow for change when new artists are playing shows in denver
   end
-  
-  def self.scrape_event_info(artist)
-    html = open(artist.url)
-    doc = Nokogiri::HTML(html)
+
+  def self.scrape_venue_info
+    site = "https://www.songkick.com/metro-areas/6404-us-denver"
+
+    doc = Nokogiri::HTML(open(site))
+
+    section = doc.css("span.venue-name")
+    section.map do |site|
+    site.css('a').text
+   end
   end
-  
+
 end
