a59e7898bf5d02ca4ae6bbf9f75db79fded4eea8
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-09 03:00:07 +0000

    cli update

--- lib/songkick/cli.rb
+++ lib/songkick/cli.rb
@@ -1,48 +1,29 @@
 # this class is my CLI controller, responsible for user interactions
 
 class SongKick::CLI
+
   def run
     Scraper.new.scrape_denver
-   binding.pry
-   display_events
-   choose_event
+    display_events
+    get_event_details
   end
 
   def display_events
     Artist.artist_name
   end
 
-  def choose_event
-    puts "Choose a number to view more details about an event:"
-    # user enters the number that corresponds with the show they'd like to view more info about
+  def get_event_details
+    puts "Choose a number to view event details:"
     input = gets.strip.to_i
-    # gets returns a string object read from standard input
-    # to_i will convert the string object to an integer
-    # strip will remove any whitespaces
-
-    # i want to scrape here to return the show details of the number selected if it is a valid input
-    # if valid_input?(input)
+    if valid_input?(input)
     puts "Great choice!"
-    # else
-    #  puts "Sorry, not a valid Choice." < shouldn't need this because it is defined in valid_input? method
-    # end
-  end
-
-  def valid_input?(input)
-    # check to see if input is between 1 and the number of artists returned based on user input
-   until valid_input.between?(1, Artist.all.size)
+    else
     puts "Sorry, not a valid choice. Choose 1 - #{Artist.all.size}"
    end
   end
 
-  def buy_tickets
-    puts "Choose a number to view ticket pricing and availability from that site:"
-    input = gets.strip.to_i
-
+  def valid_input?(input)
+    input.between?(1, Artist.all.size)
   end
-
-  # i want a loop of some sort so the program doesn't exit until being asked to
-
-
-
 end
+# i want a loop of some sort so the program doesn't exit until being asked to
--- lib/songkick/venue.rb
+++ lib/songkick/venue.rb
@@ -1,19 +1,26 @@
 class Venue
   attr_accessor :name, :address, :phone_number, :website
-
   @@all = []
 
-  def initialize
-    @@all << self
-  end
+    def initialize
+      @@all << self
+    end
 
-  def self.all
-    @@all
-  end
+    def self.all
+      @@all
+    end
 
-  def scrape_website
-    # venue = Scraper.new(@website)
-    # venue.doc.css("")
-    # venue.address
-  end
+    def scrape_website
+      # venue = Scraper.new(@website)
+      # venue.doc.css("")
+      # venue.address
+    end
+
+  def venue_info
+    s = Scraper.new(@website)
+    # .css("div.venue-info-details") = class that holds venue info
+    # .css("a.url") = venue name
+    # .css("a").attribute("href").value = venue website
+    # .css()
+    end
 end
